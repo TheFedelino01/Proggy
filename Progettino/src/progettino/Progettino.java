e857c639ab97697d6a6af29a9c96834dc83400b4
Broch Mattia <broch_mattia@LABINFO1-12.jeanmonnet.local>
2019-11-29 09:56:32 +0100

    Prova chiama

--- Progettino/src/progettino/Progettino.java
+++ Progettino/src/progettino/Progettino.java
@@ -1,10 +1,14 @@
 package progettino;
 
+import Server.VoipCall;
+import java.io.IOException;
+
 
 public class Progettino {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        VoipCall.Initialize();
     }
     
 }
