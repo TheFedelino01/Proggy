3cd71b1b451a945190b86ec9745028cd11f83ff4
tiabroch <tiabroch@gmail.com>
2019-11-29 11:54:19 +0100

    Chiama solo se paghi

--- Progettino/src/Server/VoipCall.java
+++ Progettino/src/Server/VoipCall.java
@@ -7,21 +7,25 @@ import java.net.URISyntaxException;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Call;
 import com.twilio.type.PhoneNumber;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class VoipCall {
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-    public static final String AUTH_TOKEN = "your_auth_token";
-
-    public static void main(String[] args) throws URISyntaxException {
+    public static final String ACCOUNT_SID = "AC3e19c6b63d4fb8f99c39ef34bc45e73c";
+    public static final String AUTH_TOKEN = "bf0c5676add14033dd955269840bb13c";
+    public static void chiama() throws URISyntaxException{
+        
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        String from = "+15017122661";
-        String to = "+14155551212";
+        String to = "+393703071743";
+        String from = "+393459499843";
 
         Call call = Call.creator(new PhoneNumber(to), new PhoneNumber(from),
-                new URI("http://demo.twilio.com/docs/voice.xml")).create();
+                    new URI("http://demo.twilio.com/docs/voice.xml")).create();
+        
 
         System.out.println(call.getSid());
     }
+    
 }
--- Progettino/src/progettino/Progettino.java
+++ Progettino/src/progettino/Progettino.java
@@ -2,13 +2,20 @@ package progettino;
 
 import Server.VoipCall;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Progettino {
 
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        VoipCall.Initialize();
+        try {
+            // TODO code application logic here
+            VoipCall.chiama();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(Progettino.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
