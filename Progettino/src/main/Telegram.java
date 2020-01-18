827977343d9b47c25187e335c2f74059cb4bd184
tiabroch <tiabroch@gmail.com>
2020-01-18 09:33:33 +0100

    Telegram v1.1(in teoria va ma pitorri ha il telefono)

--- Progettino/src/main/Telegram.java
+++ Progettino/src/main/Telegram.java
@@ -14,23 +14,29 @@ import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import java.io.IOException;
 import java.util.List;
 
 public class Telegram {
 
-    public static void send() {
-        TelegramBot bot = new TelegramBot("1002412328:AAErI2IPOMwyZ8bXYWnFxAHKGws1elinSPo");
+    public static void send() throws IOException {
+        // Create your bot passing the token received from @BotFather
+        String TOKEN="1002412328:AAErI2IPOMwyZ8bXYWnFxAHKGws1elinSPo";
+
+        //final byte[] audio = Telegram.class.getClassLoader().getResourceAsStream("audio.oga").readAllBytes();
+
+        TelegramBot bot = new TelegramBot(TOKEN);
+// Register for updates
         bot.setUpdatesListener(updates -> {
             // ... process updates
             // return id of last processed update or confirm them all
+            for (Update update : updates) {
+                final long id = update.message().chat().id();
+                bot.execute(new SendMessage(id, "AAAAAHAH"));
+            }
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
-        BaseRequest ree;
-        //bot.execute(request);
-        GetUpdates getUpdates = new GetUpdates().limit(100).offset(0).timeout(0);
-        GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
-        List<Update> updates = updatesResponse.updates();
-        System.out.println(updatesResponse);
+
     }
 }
 
