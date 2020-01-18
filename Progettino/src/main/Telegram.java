5d4a5c6c8cccfc7cb67e5134642621d648144aa7
tiabroch <tiabroch@gmail.com>
2020-01-18 10:18:05 +0100

    Telegram v1.2

--- Progettino/src/main/Telegram.java
+++ Progettino/src/main/Telegram.java
@@ -18,10 +18,19 @@ import java.io.IOException;
 import java.util.List;
 
 public class Telegram {
+    
+    private long id;
+    private int codDispositivo;    
+    
+    public Telegram(){
+        
+        codDispositivo=0;
+        id=0;
+    }
 
-    public static void send() throws IOException {
+    public void login() throws IOException {
         // Create your bot passing the token received from @BotFather
-        String TOKEN="1002412328:AAErI2IPOMwyZ8bXYWnFxAHKGws1elinSPo";
+        final String TOKEN="1002412328:AAErI2IPOMwyZ8bXYWnFxAHKGws1elinSPo";
 
         //final byte[] audio = Telegram.class.getClassLoader().getResourceAsStream("audio.oga").readAllBytes();
 
@@ -31,8 +40,16 @@ public class Telegram {
             // ... process updates
             // return id of last processed update or confirm them all
             for (Update update : updates) {
-                final long id = update.message().chat().id();
-                bot.execute(new SendMessage(id, "AAAAAHAH"));
+                Message message=update.message();
+                System.out.println(message.text());
+                id=message.chat().id();
+                if(message.text().equals("/login")){
+                     bot.execute(new SendMessage(id, "Inserisci il tuo"));//saccani deve parlare su un codice o CON EMAIL RICAVO UN CODICE DAL DB E LO SPARO DENTRO
+                }else if(codDispositivo==0){//codice del dispositivo = 0
+                    bot.execute(new SendMessage(id, "Registrati prima con /login"));
+                }else{
+                bot.execute(new SendMessage(id, "Non dovresti essere qui"));
+                }
             }
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
