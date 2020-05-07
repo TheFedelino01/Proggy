9115cf69bb0a9194f3d28888dbf58ec43dcc61e8
jack96489 <giacomo.orsenigo@gmail.com>
2020-05-07 12:50:52 +0200

    Aggiunto test con db

---
+++ WEBSERVICE/ProggyWebServices/src/main/java/me/proggy/proggywebservices/DbManager.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.proggy.proggywebservices;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author giaco
+ */
+public class DbManager {
+
+    private static DbManager INSTANCE;
+
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String dbms_url = "jdbc:mysql://localhost/";
+    private static final String database = "id12710500_personalsafety_new";
+    private static final String user = "root";
+    private static final String password = "";
+    private Connection connection;
+    private boolean connected;
+
+    private DbManager() {
+        String url = dbms_url + database + "?serverTimezone=Europe/Rome";
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+            connected = true;
+        } catch (SQLException | ClassNotFoundException e) {
+            connected = false;
+            e.printStackTrace();
+        }
+        System.out.println("CONFIG - " + connected);
+    }
+
+    public static synchronized DbManager getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new DbManager();
+        }
+        return INSTANCE;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+}
---
+++ WEBSERVICE/ProggyWebServices/src/main/java/me/proggy/proggywebservices/TestDbResource.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.proggy.proggywebservices;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.MediaType;
+import java.sql.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.xml.parsers.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+/**
+ * REST Web Service
+ *
+ * @author giaco
+ */
+@Path("testdb")
+public class TestDbResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of TestResource
+     */
+    public TestDbResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of
+ me.proggy.proggywebservices.TestDbResource
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getText() {
+        final DbManager db = DbManager.getInstance();
+        // verifica stato connessione a DBMS
+        if (!db.isConnected()) {
+            throw new WebApplicationException("DBMS server error!", 500);
+        }
+
+        try {
+            String sql = "SELECT * FROM scheda";
+            Statement statement = db.getConnection().createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            StringBuilder ris = new StringBuilder();
+            ris.append("<scheda>");
+            while (result.next()) {
+                ris.append("<id>").append(result.getString(1)).append("</id>");
+            }
+            ris.append("</scheda>");
+            result.close();
+            statement.close();
+
+            return ris.toString();
+
+        } catch (SQLException e) {
+            throw new WebApplicationException("DBMS server error!", 500);
+        }
+    }
+
+    /**
+     * PUT method for updating or creating an instance of TestDbResource
+     *
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void putText(String content) {
+    }
+}
