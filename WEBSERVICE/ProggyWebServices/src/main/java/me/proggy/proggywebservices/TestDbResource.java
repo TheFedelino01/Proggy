8ab802ed3d0adb620d8dd900d50a6f60da70684b
jack96489 <giacomo.orsenigo@gmail.com>
2020-05-08 12:52:40 +0200

    Prova inserimento nel db e test automatizzati

--- WEBSERVICE/ProggyWebServices/src/main/java/me/proggy/proggywebservices/TestDbResource.java
+++ WEBSERVICE/ProggyWebServices/src/main/java/me/proggy/proggywebservices/TestDbResource.java
@@ -5,18 +5,23 @@
  */
 package me.proggy.proggywebservices;
 
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.MediaType;
-import java.sql.*;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.xml.parsers.*;
-
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
+import javax.ws.rs.core.UriInfo;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * REST Web Service
@@ -36,10 +41,9 @@ public class TestDbResource {
     }
 
     /**
-     * Retrieves representation of an instance of
- me.proggy.proggywebservices.TestDbResource
+     * Esempio di richiesta GET (ritorna elenco schede)
      *
-     * @return an instance of java.lang.String
+     * @return stringa xml con elenco delle schede
      */
     @GET
     @Produces(MediaType.APPLICATION_XML)
@@ -50,18 +54,16 @@ public class TestDbResource {
             throw new WebApplicationException("DBMS server error!", 500);
         }
 
-        try {
-            String sql = "SELECT * FROM scheda";
-            Statement statement = db.getConnection().createStatement();
-            ResultSet result = statement.executeQuery(sql);
+        final String sql = "SELECT * FROM scheda";
+        try (Statement statement = db.getConnection().createStatement();
+             ResultSet result = statement.executeQuery(sql)) {
+
             StringBuilder ris = new StringBuilder();
             ris.append("<scheda>");
             while (result.next()) {
                 ris.append("<id>").append(result.getString(1)).append("</id>");
             }
             ris.append("</scheda>");
-            result.close();
-            statement.close();
 
             return ris.toString();
 
@@ -78,5 +80,54 @@ public class TestDbResource {
     @PUT
     @Consumes(MediaType.TEXT_PLAIN)
     public void putText(String content) {
+    }
+
+    /**
+     * Esempio di richiesta POST (inserimento nuova scheda)
+     *
+     * @param xml id dell'ente a cui appartiene
+     * @return id elemento inserito
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     */
+    @POST
+    @Consumes({MediaType.APPLICATION_XML})
+    @Produces(MediaType.TEXT_PLAIN)
+    public String createScheda(String xml) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        Document document = builder.parse(is);
+        final String ente = document.getElementsByTagName("idEnte").item(0).getFirstChild().getNodeValue();
+
+        final DbManager db = DbManager.getInstance();
+        // verifica stato connessione a DBMS
+        if (!db.isConnected()) {
+            throw new WebApplicationException("DBMS server error!", 500);
+        }
+        long id = -1;
+        final String sql = "INSERT INTO scheda (idEnte) VALUES (?)";
+        try (PreparedStatement statement = db.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, ente);
+            int affectedRows = statement.executeUpdate();
+            // check the affected rows
+            if (affectedRows > 0) {
+                // get the ID back
+                try (ResultSet rs = statement.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        id = rs.getLong(1);
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    throw new WebApplicationException("DBMS server error!", 500);
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new WebApplicationException("DBMS server error!", 500);
+        }
+        return "Inserimento effettuato. ID: " + id;
+
     }
 }
