9ba538fd87a6fb74135384a40e13131da48007e4
TheFedelino01 <federico.saccani01@gmail.com>
2020-05-10 16:36:59 +0200

    Corretto metodo POST

--- WEBSERVICE/ProggyWebServices/src/main/java/saccani/DevicesAssocia.java
+++ WEBSERVICE/ProggyWebServices/src/main/java/saccani/DevicesAssocia.java
@@ -46,6 +46,7 @@ public class DevicesAssocia {
      *
      * @param idScheda id della scheda da associare
      * @param idUtente id dell'utente
+     * @param dataOra ]Opzionale] la data e l'ora dell'associazione (esempio: /api/devices/associa/1/1/2012-07-10%2014:58:00.000000)
      * @return xml con il risultato dell'operazione
      */
     @POST
@@ -58,79 +59,40 @@ public class DevicesAssocia {
             Element root = XMLUtils.loadXmlFromString(content);
             int idScheda = Integer.parseInt(root.getElementsByTagName("idScheda").item(0).getTextContent());
             int idUtente = Integer.parseInt(root.getElementsByTagName("idUtente").item(0).getTextContent());
-
+            String dataOra="";
+            java.util.Date dateObj=new java.util.Date();
+            
+            //Se ha specificato il parametro opzionale
+            if(root.getElementsByTagName("dataOra").getLength()>0){
+                dataOra = root.getElementsByTagName("dataOra").item(0).getTextContent();
+                
+                try{
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+                    dateObj = sdf.parse(dataOra);
+                }catch(Exception e){
+                    return "<result>406</result>";
+                }
+            }
+            
             // verifica stato connessione a DBMS
             if (!db.isConnected()) {
                 System.err.println("DB non connesso");
                 throw new WebApplicationException("DB non connesso!", 500);
             }
-
-
-            String sql = "INSERT INTO `utilizza` (`idScheda`, `idCliente`) VALUES (?,?);";
-            PreparedStatement statement = db.getConnection().prepareStatement(sql);
+            
+            PreparedStatement statement;
+            if(dataOra.isEmpty()){
+                String sql = "INSERT INTO `utilizza` (`idScheda`, `idCliente`) VALUES (?,?);";
+                statement = db.getConnection().prepareStatement(sql);
                 statement.setInt(1, idScheda);
                 statement.setInt(2, idUtente);
-
-                int i = statement.executeUpdate();
-                if (i > 0) {
-                    //throw new WebApplicationException("Success", 200); Non visualizza niente
-                    return "<result>200</result>";
-                } else {
-                    //throw new WebApplicationException("Wrong parameters", 406); Non visualizza niente
-                    return "<result>"
-                            + "<status>406</status>"
-                            + "<errore>Parametri inseriti errati</errore>"
-                            + "</result>";
-                }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new WebApplicationException("Wrong parameters", 406);
-        }
-    }
-
-
-    /**
-     * Consente di associare la scheda con uno specifico utente specificando l'ora e la data dell'associazione
-     *
-     * @param idScheda id della scheda da associare
-     * @param idUtente id dell'utente
-     * @param dataOra la data e l'ora dell'associazione (esempio: /api/devices/associa/1/1/2012-07-10%2014:58:00.000000)
-     * @return xml con il risultato dell'operazione
-     */
-    @POST
-    @Produces(MediaType.APPLICATION_XML)
-    @Consumes("application/xml")
-    public String getXmlSchedaOptional(String content){
-        //@Path("{idScheda}/{idUtente}/{dataOra}")
-        //@PathParam(value = "idScheda") int idScheda,@PathParam(value = "idUtente") int idUtente,@PathParam(value = "dataOra") String dataOra
-        final DbManager db = DbManager.getInstance();
-        // verifica stato connessione a DBMS
-        if (!db.isConnected()) {
-            System.err.println("DB non connesso");
-            throw new WebApplicationException("DB non connesso!", 500);
-        }
-        
-        Element root = XMLUtils.loadXmlFromString(content);
-        int idScheda = Integer.parseInt(root.getElementsByTagName("idScheda").item(0).getTextContent());
-        int idUtente = Integer.parseInt(root.getElementsByTagName("idUtente").item(0).getTextContent());
-        String dataOra = root.getElementsByTagName("dataOra").item(0).getTextContent();
-            
-        //Esempio funzionante: /api/devices/associa/1/1/2012-07-10%2014:58:00.000000
-        java.util.Date dateObj;
-        try{
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
-            dateObj = sdf.parse(dataOra);
-        }catch(Exception e){
-            return "<result>406</result>";
-        }
-        
-        String sql = "INSERT INTO `utilizza` (`idScheda`, `idCliente`,`dataOraInizio`) VALUES (?,?,?);";
-        try (PreparedStatement statement = db.getConnection().prepareStatement(sql)) {
-            statement.setInt(1, idScheda);
-            statement.setInt(2, idUtente);
-            statement.setTimestamp(3, new Timestamp(dateObj.getTime()));
+            }else{
+                String sql = "INSERT INTO `utilizza` (`idScheda`, `idCliente`,`dataOraInizio`) VALUES (?,?,?);";
+                statement = db.getConnection().prepareStatement(sql);
+                statement.setInt(1, idScheda);
+                statement.setInt(2, idUtente);
+                statement.setTimestamp(3, new Timestamp(dateObj.getTime()));
+            }
             
             int i = statement.executeUpdate();
             if (i > 0) {
@@ -145,10 +107,10 @@ public class DevicesAssocia {
             }
 
 
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             throw new WebApplicationException("Wrong parameters", 406);
         }
-    }
+    } 
 
 }
